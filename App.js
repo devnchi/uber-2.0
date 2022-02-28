0baffbe74b5092c085aee393ef585e0f1eca38fd
Chi Igbokwe <ch11gbokwedevelopment@gmail.com>
2022-02-28 11:11:05 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+// import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+export default function App() {
+  const Stack = createStackNavigator();
+  
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen 
+            name='Home' 
+            component={HomeScreen} 
+            options={{
+              headerShown: false,
+            }} 
+            />
+            <Stack.Screen 
+            name='Map' 
+            component={MapScreen} 
+            options={{
+              headerShown: false,
+            }} 
+            />
+          </Stack.Navigator>
+          <HomeScreen />
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ screens/DeliveryScreen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import DeliveryScreen from './DeliveryScreen';
+
+const DeliveryScreen = () => {
+  return (
+    <View>
+      <Text>DeliveryScreen</Text>
+    </View>
+  )
+}
+
+export default DeliveryScreen;
+
+const styles = StyleSheet.create({})
